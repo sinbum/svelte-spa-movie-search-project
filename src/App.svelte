a89c68a5847cb2dd377fc5df057e337faad6c652
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-15 18:03:37 +0900

    Footer 컴포넌트 작성.

--- src/App.svelte
+++ src/App.svelte
@@ -2,11 +2,12 @@
     import Router from "svelte-spa-router"
     import Header from "~/components/Header.svelte";
     import routes from '~/routes'
+    import Footer from "~/components/Footer.svelte";
 </script>
 
 <Header/>
-
 <Router {routes}/>
+<Footer/>
 
 
 
---
+++ src/components/Footer.svelte
@@ -0,0 +1,27 @@
+<footer>
+    <div>
+        <a href="https://github.com/sinbum">GitHub Repo.</a>
+    </div>
+    <div>
+        <a href="#">@{new Date().getFullYear()} SINBUM</a>
+    </div>
+
+</footer>
+
+
+<style lang="scss">
+  footer {
+    padding: 70px;
+    text-align: center;
+
+    a {
+      color: $color--white-10;
+      text-decoration: none;
+
+      &:hover {
+        text-decoration: underline;
+      }
+    }
+
+  }
+</style>
