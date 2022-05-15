2bce7df3f3ab6c02d1d8dbbf253e02071bbb5d68
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-15 16:01:19 +0900

    spa 라우츠 링크와 하이라이팅(active활성화)

--- src/component/Header.svelte
+++ src/component/Header.svelte
@@ -1,3 +1,36 @@
+<script>
+    import {link} from 'svelte-spa-router'
+    import active from 'svelte-spa-router/active'
+
+</script>
+
 <header>
     Header!
+    <!--  루트경로는 {'/'} 가 필요없음. active시에.  -->
+    <a
+            use:link
+            use:active
+            href="/">
+        Home
+    </a>
+
+    <a
+            use:link
+            use:active={'/about'}
+            href="/about">
+        about
+    </a>
 </header>
+
+<style lang="scss">
+  header {
+    a {
+      &.active {
+        color: red;
+      }
+    }
+  }
+  header :global(a.active) {
+    color: red;
+  }
+</style>
