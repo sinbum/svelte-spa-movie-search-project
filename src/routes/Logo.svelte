16fee914c6eac389a61551574c421be34bdf2434
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-15 16:20:56 +0900

    표준 css rgba 함수는 rgba(r,g,b,a)로작성
    scss 환경 표준
    rgba 는 인수갯수에 따라서 다르다.
    인수 4개 > 표준 css함수 동작
    인수 2개 > 표준 scss함수 0동작

---
+++ src/routes/Logo.svelte
@@ -0,0 +1,24 @@
+<script>
+    import {link} from 'svelte-spa-router'
+
+</script>
+
+<a
+        use:link
+        href="/">
+    <span>OMDBAPI</span>.COM
+</a>
+
+
+<style lang="scss">
+    a{
+      display: block;
+      font-family: 'Oswald',sans-serif;
+      font-size: 30px;
+      color: rgba(#fff,.5);
+      text-decoration: none; //글씨 밑줄 사용X
+      span {
+        color: #FDC000;
+      }
+    }
+</style>
