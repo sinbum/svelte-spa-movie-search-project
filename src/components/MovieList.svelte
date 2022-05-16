8e5d1ceb7147bcab31cc9204e6ac67f4a6ccfb39
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-16 23:14:54 +0900

    목록으로 출력될 영화 카드 만들기

--- src/components/MovieList.svelte
+++ src/components/MovieList.svelte
@@ -1,11 +1,30 @@
 <script>
     import { movies } from '~/store/movies'
+    import MovieCard from "~/components/MovieCard.svelte";
+
 </script>
 
 <div class="movie-list">
     <div class="movies">
         {#each $movies as movie (movie.imdbID)}
-            {movie.Title}
+            <MovieCard {movie}/>
+<!--            {movie.Title}-->
         {/each}
     </div>
 </div>
+
+<style lang="scss">
+    .movie-list{
+      margin-top: 30px;
+      padding: 10px;
+      background-color: $color--area;
+      border-radius: 4px;
+      text-align: center;
+
+      .movies{
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: center;
+      }
+    }
+</style>
