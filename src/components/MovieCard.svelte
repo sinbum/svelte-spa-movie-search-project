24a744c2797c642a2fa9a64b30521b959d98fb22
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-17 22:51:40 +0900

    컨텐츠 이미지 렌더링 전 로딩 애니메이션 추가

--- src/components/MovieCard.svelte
+++ src/components/MovieCard.svelte
@@ -1,13 +1,33 @@
 <script>
     import {link} from 'svelte-spa-router'
+    import Loader from '~/components/Loader.svelte'
+
+    let imageLoading = true;
 
     export let movie
+
+    if(movie.Poster === 'N/A'){
+        imageLoading = false
+    }else{
+        const img = document.createElement('img')
+        img.src = movie.Poster
+        img.addEventListener('load',()=>{
+            imageLoading = false
+        })
+    }
 </script>
 
 <a
         use:link
         href={'/movie/${movie.imdbID}'}
         class="movie">
+    {#if imageLoading}
+        <Loader
+                scale=".5"
+                absolute
+        />
+
+    {/if}
     <div
             class="poster"
             style="background-image: url({movie.Poster});">
@@ -27,96 +47,99 @@
         <div class="title">{movie.Title}</div>
 
     </div>
-    
+
 </a>
 
 <style lang="scss">
-    .movie{
-      display: block;
-      width: 200px;
-      height: 300px;
-      margin: 10px;
-      border-radius: 6px;
-      text-decoration: none;
-      overflow: hidden;
-      cursor: pointer;
-      position: relative;
-      &:hover{
-        &::after{
-          content: "";
-          position: absolute;
-          top: 0;
-          left: 0;
-          width: 100%;
-          height: 100%;
-          border: 6px solid $color--primary;
-          box-sizing: border-box;
-        }
-      }
+  .movie {
+    display: block;
+    width: 200px;
+    height: 300px;
+    margin: 10px;
+    border-radius: 6px;
+    text-decoration: none;
+    overflow: hidden;
+    cursor: pointer;
+    position: relative;
 
-
-      .poster{
+    &:hover {
+      &::after {
+        content: "";
+        position: absolute;
+        top: 0;
+        left: 0;
         width: 100%;
         height: 100%;
-        background-color: $color--area;
-        background-position: center;
-        background-size: cover;
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        font-family: 'Oswald',sans-serif;
-        color: $color--white-5;
-        font-size: 20px;
-        text-align: center;
-      }
-      .info{
-        width: 100%;
-        height: 66px;
-        padding: 14px;
+        border: 6px solid $color--primary;
         box-sizing: border-box;
-        overflow: hidden;
+      }
+    }
+
+
+    .poster {
+      width: 100%;
+      height: 100%;
+      background-color: $color--area;
+      background-position: center;
+      background-size: cover;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      font-family: 'Oswald', sans-serif;
+      color: $color--white-5;
+      font-size: 20px;
+      text-align: center;
+    }
+
+    .info {
+      width: 100%;
+      height: 66px;
+      padding: 14px;
+      box-sizing: border-box;
+      overflow: hidden;
+      position: absolute;
+      bottom: 0;
+      left: 0;
+
+      .poster {
         position: absolute;
         bottom: 0;
         left: 0;
+        transform: scale(2);
+        filter: blur(5px);
 
-        .poster {
+
+        &::after {
+          content: "";
+          background-color: $color--black-50;
           position: absolute;
-          bottom: 0;
+          top: 0;
           left: 0;
-          transform:scale(2);
-          filter: blur(5px);
-
-
-          &::after {
-            content: "";
-            background-color: $color--black-50;
-            position: absolute;
-            top: 0;
-            left: 0;
-            width: 200%;
-            height: 200%;
-          }
+          width: 200%;
+          height: 200%;
         }
       }
-      .year{
-        position: relative;
-        color: $color--primary;
-        font-size: 12px;
-      }
-      .title{
-        position: relative;
-        font-size: 15px;
-        font-family: 'Oswald',sans-serif;
-        color: $color--white;
-        overflow: hidden;
-        text-overflow: ellipsis;
-        white-space: nowrap;
+    }
 
-      }
+    .year {
+      position: relative;
+      color: $color--primary;
+      font-size: 12px;
+    }
 
+    .title {
+      position: relative;
+      font-size: 15px;
+      font-family: 'Oswald', sans-serif;
+      color: $color--white;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      white-space: nowrap;
 
     }
 
+
+  }
 
 
 </style>
