144c1a7778222b0a78503731839b8d3faff83821
Sinbum <79790395+sinbum@users.noreply.github.com>
2022-05-18 00:27:39 +0900

    스켈레톤 UI . 코드자동정리 .2

--- src/components/Loader.svelte
+++ src/components/Loader.svelte
@@ -2,12 +2,12 @@
     export let scale
     export let absolute
     export let fixed
+
 </script>
 
-<!--https://tobiasahlin.com/spinkit/-->
 
 <div
-        style="transform: scale({scale});"
+        style="transform: scale({scale})"
         class:absolute
         class:fixed
         class="loading">
@@ -19,120 +19,120 @@
         <div class="sk-chase-dot"></div>
         <div class="sk-chase-dot"></div>
     </div>
+
 </div>
 
 <style lang="scss">
-  .loading {
-    width: 40px;
-    height: 40px;
-    margin: auto;
-    padding: 30px;
-    top: 0;
-    left: 0;
-    bottom: 0;
-    right: 0;
+    .loading{
+      width: 40px;
+      height: 40px;
+      padding: 30px;
+      top: 0;
+      left: 0;
+      bottom: 0;
+      right: 0;
+      margin: auto;
 
-    &.absolute {
-      position: absolute;
+      &.absolute{
+        position: absolute;
+      }
+      &.fixed{
+        position:fixed;
+      }
     }
 
-    &.fixed {
-      position: fixed;
+    .sk-chase {
+        width: 40px;
+        height: 40px;
+        position: relative;
+        animation: sk-chase 2.5s infinite linear both;
     }
-  }
 
-  .sk-chase {
-    width: 40px;
-    height: 40px;
-    position: relative;
-    animation: sk-chase 2.5s infinite linear both;
-  }
-
-  .sk-chase-dot {
-    width: 100%;
-    height: 100%;
-    position: absolute;
-    left: 0;
-    top: 0;
-    animation: sk-chase-dot 2.0s infinite ease-in-out both;
-  }
-
-  .sk-chase-dot:before {
-    content: '';
-    display: block;
-    width: 25%;
-    height: 25%;
-    background-color: $color--primary;
-    border-radius: 100%;
-    animation: sk-chase-dot-before 2.0s infinite ease-in-out both;
-  }
-
-  .sk-chase-dot:nth-child(1) {
-    animation-delay: -1.1s;
-  }
-
-  .sk-chase-dot:nth-child(2) {
-    animation-delay: -1.0s;
-  }
-
-  .sk-chase-dot:nth-child(3) {
-    animation-delay: -0.9s;
-  }
-
-  .sk-chase-dot:nth-child(4) {
-    animation-delay: -0.8s;
-  }
-
-  .sk-chase-dot:nth-child(5) {
-    animation-delay: -0.7s;
-  }
-
-  .sk-chase-dot:nth-child(6) {
-    animation-delay: -0.6s;
-  }
-
-  .sk-chase-dot:nth-child(1):before {
-    animation-delay: -1.1s;
-  }
-
-  .sk-chase-dot:nth-child(2):before {
-    animation-delay: -1.0s;
-  }
-
-  .sk-chase-dot:nth-child(3):before {
-    animation-delay: -0.9s;
-  }
-
-  .sk-chase-dot:nth-child(4):before {
-    animation-delay: -0.8s;
-  }
-
-  .sk-chase-dot:nth-child(5):before {
-    animation-delay: -0.7s;
-  }
-
-  .sk-chase-dot:nth-child(6):before {
-    animation-delay: -0.6s;
-  }
-
-  @keyframes sk-chase {
-    100% {
-      transform: rotate(360deg);
+    .sk-chase-dot {
+        width: 100%;
+        height: 100%;
+        position: absolute;
+        left: 0;
+        top: 0;
+        animation: sk-chase-dot 2.0s infinite ease-in-out both;
     }
-  }
 
-  @keyframes sk-chase-dot {
-    80%, 100% {
-      transform: rotate(360deg);
+    .sk-chase-dot:before {
+        content: '';
+        display: block;
+        width: 25%;
+        height: 25%;
+        background-color: $color--primary;
+        border-radius: 100%;
+        animation: sk-chase-dot-before 2.0s infinite ease-in-out both;
     }
-  }
 
-  @keyframes sk-chase-dot-before {
-    50% {
-      transform: scale(0.4);
+    .sk-chase-dot:nth-child(1) {
+        animation-delay: -1.1s;
     }
-    100%, 0% {
-      transform: scale(1.0);
+
+    .sk-chase-dot:nth-child(2) {
+        animation-delay: -1.0s;
     }
-  }
+
+    .sk-chase-dot:nth-child(3) {
+        animation-delay: -0.9s;
+    }
+
+    .sk-chase-dot:nth-child(4) {
+        animation-delay: -0.8s;
+    }
+
+    .sk-chase-dot:nth-child(5) {
+        animation-delay: -0.7s;
+    }
+
+    .sk-chase-dot:nth-child(6) {
+        animation-delay: -0.6s;
+    }
+
+    .sk-chase-dot:nth-child(1):before {
+        animation-delay: -1.1s;
+    }
+
+    .sk-chase-dot:nth-child(2):before {
+        animation-delay: -1.0s;
+    }
+
+    .sk-chase-dot:nth-child(3):before {
+        animation-delay: -0.9s;
+    }
+
+    .sk-chase-dot:nth-child(4):before {
+        animation-delay: -0.8s;
+    }
+
+    .sk-chase-dot:nth-child(5):before {
+        animation-delay: -0.7s;
+    }
+
+    .sk-chase-dot:nth-child(6):before {
+        animation-delay: -0.6s;
+    }
+
+    @keyframes sk-chase {
+        100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    @keyframes sk-chase-dot {
+        80%, 100% {
+            transform: rotate(360deg);
+        }
+    }
+
+    @keyframes sk-chase-dot-before {
+        50% {
+            transform: scale(0.4);
+        }
+        100%, 0% {
+            transform: scale(1.0);
+        }
+    }
 </style>
