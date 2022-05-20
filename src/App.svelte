e5558facb0120585ca8889b77c42deeca0e207f4
sinbum <sinbum@kakao.com>
2022-05-20 11:08:39 +0900

    페이지 전환 효과 적용 (Fade)

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,17 @@
 <script>
-    import Router from "svelte-spa-router"
+    import { fade } from 'svelte/transition'
+    import Router,{ location } from "svelte-spa-router"
     import Header from "~/components/Header.svelte";
     import routes from '~/routes'
     import Footer from "~/components/Footer.svelte";
 </script>
 
 <Header/>
-<Router {routes}/>
+{#key $location}
+    <div in:fade>
+        <Router {routes}/>
+    </div>
+{/key}
 <Footer/>
 
 
